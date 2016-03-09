7de0354be62395adf7914393b667508ab06c5586
Adam Suban-Loewen <adam.subanloewen@gmail.com>
2016-03-09 18:43:26 -0500

    added bullsage command

--- plugins/lol/lol.plugin.zsh
+++ plugins/lol/lol.plugin.zsh
@@ -5,6 +5,7 @@ alias mangocode='vi /Users/Atom/.oh-my-zsh/themes/mangocode.zsh-theme'
 
 alias wanip='dig +short myip.opendns.com @resolver1.opendns.com'
 alias cantconnect='wanip'
+alias bullsage='fortune | cowsay'
 
 alias wtf='dmesg'
 alias onoz='cat /var/log/errors.log'
