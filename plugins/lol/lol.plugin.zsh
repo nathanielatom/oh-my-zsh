81554c84f52f108c1fa591785cdf822e4252b043
Adam Suban-Loewen <adam.subanloewen@gmail.com>
2017-03-15 11:56:12 -0400

    added diff = git diff alias

--- plugins/lol/lol.plugin.zsh
+++ plugins/lol/lol.plugin.zsh
@@ -5,6 +5,7 @@ alias mangocode='vi /Users/Atom/.oh-my-zsh/themes/mangocode.zsh-theme'
 
 alias pip='noglob pip'
 alias -g chrome='/Applications/Google\ Chrome.app'
+alias diff='git diff'
 
 alias wanip='dig +short myip.opendns.com @resolver1.opendns.com'
 alias cantconnect='wanip'
